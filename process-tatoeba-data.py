061dc3362e78e6aaebf65a89b38974eb7311e454
pallavip11 <46246179+pallavip11@users.noreply.github.com>
2021-04-15 21:33:16 +0530

    Update process-tatoeba-data.py

--- process-tatoeba-data.py
+++ process-tatoeba-data.py
@@ -4,12 +4,12 @@ import re
 def main():
     # Split the original dataset into parallel English and French txts
     eng_sentences = []
-    fra_sentences = []
+    hin_sentences = []
     with open('data/raw/hin.txt') as f:
         for line in f:
             line = re.split(r'\t+', line)
             eng_sentences.append(line[0])
-            fra_sentences.append(line[1])
+            hin_sentences.append(line[1])
 
     with open('data/raw/english.txt', 'w') as f:
         for line in eng_sentences:
